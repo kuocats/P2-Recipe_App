4b6cf18c653ce7eee59dc357902b9f3dd4ed9c74
Bradford <bzlee@ucdavis.edu>
2023-06-28 22:22:53 -0700

    added handlebars files, user login files, edited server.js files, downloaded npm packages

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const categoryRoutes = require("./category-routes");
 const recipesRoutes = require("./recipes-routes");
 const ingredientsRoutes = require("./ingredients-routes");
+const userRoutes = require('./user-routes');
 
 router.use("/categories", categoryRoutes);
 router.use("/recipes", recipesRoutes);
 router.use("/ingredients", ingredientsRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
